94de489054d8c541bc38b03e554539eac1362b90
Danil Tolmachov <danil.tolmachov45@gmail.com>
2023-07-29 21:48:56 +0300

    Implement the functionality of demo page

--- script.js
+++ script.js
@@ -1,32 +1,65 @@
-const video = document.getElementById('video')
+const video = document.getElementById('video');
+const emojiContainer = document.getElementById('emojiContainer'); // Emoji container 
+
+const emojiMap = {
+  neutral: '😐',
+  happy: '😄',
+  sad: '😢',
+  angry: '😠',
+  surprised: '😲',
+  disgusted: '🤢',
+  fearful: '😨'
+};
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+  faceapi.nets.tinyFaceDetector.loadFromUri('/team-16-front-react/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/team-16-front-react/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/team-16-front-react/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/team-16-front-react/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/team-16-front-react/models')
+]).then(startVideo);
 
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+  try {
+    navigator.mediaDevices.getUserMedia({ video: {} })
+      .then(stream => {
+        video.srcObject = stream;
+      })
+      .catch(err => console.error("Error accessing the camera: ", err));
+  } catch {
+    console.error("getUserMedia is not available");
+  }
+}
+
+async function getCurrentEmotion(video) {
+  let expressions = (await faceapi.detectSingleFace(video).withFaceExpressions()).expressions;
+  let resultExpression = 'neutral'
+
+  // Get the most possible emotion
+  for (let key in expressions) {
+    let value = expressions[key]
+
+    if (value > expressions[resultExpression]) {
+      resultExpression = key
+    }
+  }
+  return resultExpression;
 }
 
 video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    let emotion = await getCurrentEmotion(video)
     
-  }, 100)
-})
+    // Выводим эмодзи, если обнаружено не нейтральное выражение
+    if (emotion !== 'neutral') {
+      emojiContainer.innerHTML = emojiMap[emotion];
+    } else {
+      emojiContainer.innerHTML = '';
+    }
+  }, 100);
+});
